91ca624848905799ffd3cd07d9c6eb2afc1dd94d
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-10 15:37:11 -0700

    Project work update ...

---
+++ GC_SDE_Course_Python_Work_01/InClass_01/Point01.py
@@ -0,0 +1,23 @@
+# class Point:
+#     pass
+#
+# def reset(self):
+#     self.x = 0
+#     self.y = 0
+#
+# p3 = Point()
+#
+# # can dynamically define the fields of this class as below, without
+# # pre-defining the fields of the class etc..
+# #  From here
+# # p1 = Point()
+# # p2 = Point()
+# #
+# # p1.x = 5
+# # p1.y = 4
+# #
+# # p2.x = 3
+# # p2.y = 6
+# #  To here
+# # print(p1.x, p1.y)
+# # print(p1.x, p1.y)
---
+++ GC_SDE_Course_Python_Work_01/InClass_01/Point02.py
@@ -0,0 +1,25 @@
+class Point02:
+    # A constructor
+    def __init__(self):
+        self.y = 1
+        self.x = 1
+        # self.move(x, y)
+    # pass
+
+
+def reset(self):
+    self.x = 0
+    self.y = 0
+
+
+def move(self, x, y):
+    self.x = x
+    self.y = y
+
+
+move(2, 3)
+
+p1 = Point02()
+
+print()
+print("Point02 (", p1.x, ",", p1.y, ")")
