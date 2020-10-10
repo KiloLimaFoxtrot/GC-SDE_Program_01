952412b745700cf67bd165b64308e1e5cabb532e
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-10 15:50:21 -0700

    Project work update ...

--- GC_SDE_Course_Python_Work_01/InClass_01/Point02.py
+++ GC_SDE_Course_Python_Work_01/InClass_01/Point02.py
@@ -2,22 +2,18 @@ class Point02:
     # A constructor
     def __init__(self):
         self.y = 1
-        self.x = 1
-        # self.move(x, y)
-    # pass
-
+        self.x = 1  # self.move(x, y)  # pass
 
 def reset(self):
     self.x = 0
     self.y = 0
 
-
 def move(self, x, y):
     self.x = x
     self.y = y
 
-
-move(2, 3)
+#  ? not working
+# move(2, 3)
 
 p1 = Point02()
 
