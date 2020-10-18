df07e8585216d20d6564589d2f924a6733ca4042
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-17 20:45:15 -0700

    Project work update ...

--- GC_SDE_Course_Python_Work_01/InClass_01/MyFirstCLass.py
+++ GC_SDE_Course_Python_Work_01/InClass_01/MyFirstCLass.py
@@ -1,3 +1,14 @@
 class MyFirstClass:
     pass
 
+# This is a sample Python script for practicing unit tests
+
+def print_hi(name):
+    print("Hi, ", name)
+
+
+def sum_from_one_through_n(n):
+    sum = 0
+    i = 1
+    while i <= n:
+        sum += i
